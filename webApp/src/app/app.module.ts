f3e977fc1f3aa6f727b6edcc3706c9ad2eec83a0
Richard Nagy <95.richard.nagy@gmail.com>
2022-08-06 18:59:47 +0200

    [#6] [#20] Setup Angular Routing module and create custom 404 page

--- webApp/src/app/app.module.ts
+++ webApp/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainContentComponent } from './main-content/main-content.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContentComponent,
+    PrivacyPolicyComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
