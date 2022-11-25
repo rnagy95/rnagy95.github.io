e03d5f2597efefa03d7124ceb9a71616c82c9972
Richard Nagy <95.richard.nagy@gmail.com>
2022-11-25 07:03:04 +0100

    Create the Footer module. Add Terms & Conditions module (emtpy yet) to the project

--- webApp/src/app/main-content/main-content.component.ts
+++ webApp/src/app/main-content/main-content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Link } from '../interfaces/Link';
+import { ContactLink } from '../interfaces/ContactLink';
 
 @Component({
   selector: 'app-main-content',
@@ -9,6 +10,7 @@ import { Link } from '../interfaces/Link';
 export class MainContentComponent implements OnInit {
 
   links:Link[] = [{href: '', text: 'about'}, {href: '', text: 'technologies'}, {href: '', text: 'experience'}]
+  contacts: ContactLink[] = [{altText: 'E-Mail', logo:'../../assets/email.png', url: "mailto:95.richard.nagy@gmail.com"}, {altText: 'GitHub', logo:'../../assets/GitHub-Mark-Light-64px.png', url: "https://github.com/rnagy95"}, {altText: 'LinkedIn', logo:'../../assets/LI-In-Bug.png', url: "https://www.linkedin.com/in/richard-nagy-636310135/"}]
 
   constructor() { }
 
