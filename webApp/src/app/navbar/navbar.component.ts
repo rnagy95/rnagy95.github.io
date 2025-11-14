8a90e18d9d61663ac3742acca341a5f98de3fd02
rnagy95 <95.richard.nagy@gmail.com>
2025-11-14 02:02:22 +0100

    Redesign NavBar component

--- webApp/src/app/navbar/navbar.component.ts
+++ webApp/src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { Link } from '../interfaces/Link';
 import { LocalizationService } from '../services/localization/localization.service';
+import { NavmenuService } from '../services/navmenu/navmenu.service';
 
 @Component({
     selector: 'app-navbar',
@@ -11,10 +12,18 @@ import { LocalizationService } from '../services/localization/localization.servi
 export class NavBarComponent implements OnInit {
 
   @Input() links: Link[] | undefined;
+  @Input() sidenav: Boolean | undefined;
 
-  constructor(public localizationService: LocalizationService) { }
+  public onTop: boolean = true;
+
+  constructor(public localizationService: LocalizationService, public navmenuService: NavmenuService) { }
 
   ngOnInit(): void {
+     this.onScroll();
   }
 
+  @HostListener('window:scroll', ['$event'])
+  public onScroll(): void { 
+    this.onTop = window.scrollY === 0;
+  }
 }
