cffb5972f87d30c48cc3adf012cf94ac15f8f29d
rnagy95 <95.richard.nagy@gmail.com>
2026-03-25 22:54:40 +0100

    Revert "Fixes package version format"
    This reverts commit 7cb851795f19fb7d39f58930d9627fb5e4e54d28.

--- webApp/generate-version-file.js
+++ webApp/generate-version-file.js
@@ -1,7 +1,7 @@
 const { version } = require('./package.json');
 const fs = require('fs');
 
-const content = `{"APP_VERSION": "v${version}"}`;
+const content = `{"APP_VERSION": "${version}"}`;
 
 fs.writeFileSync('src/assets/version.json', content);
 console.log('Version file generated:', version);
