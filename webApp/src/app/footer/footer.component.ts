999306002a6f38dd74e97d2f4e4bcbbb1f21a8ab
rnagy95 <95.richard.nagy@gmail.com>
2026-03-25 22:55:37 +0100

    Revert "Implement code versioning"
    This reverts commit fc8589517ee4f39572644e71263a71f53d3555c3.

--- webApp/src/app/footer/footer.component.ts
+++ webApp/src/app/footer/footer.component.ts
@@ -5,7 +5,6 @@ import { CookiePopupComponent } from '../cookie-popup/cookie-popup.component';
 import { ContactLink } from '../interfaces/ContactLink';
 import { PrintService } from '../services/print/print.service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import version from '../../assets/version.json';
 
 @Component({
     selector: 'app-footer',
@@ -17,7 +16,7 @@ export class FooterComponent implements OnInit {
 
   @Input() contacts: ContactLink[] | undefined;
 
-  public version: string = version.APP_VERSION;
+  public version: string = 'v1.0.0';
 
   constructor(public localizationService: LocalizationService, public printService: PrintService, private dialog: MatDialog, private domSanitizer: DomSanitizer) { }
 
